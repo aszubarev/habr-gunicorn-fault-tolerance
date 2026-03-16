dd0a8197746df745d5549bf9cdec8a1c60377607
aszubarev <aszubarev@domclick.ru>
2026-03-16 22:12:40 +0300

    init

--- examples/0-fixed-by-separate-networks/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    netstat -p tcp -na | grep 8000 | grep TIME_WAIT | wc -l | while IFS= read -r line; do
-        echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line"
-    done
-    sleep 1
-done
